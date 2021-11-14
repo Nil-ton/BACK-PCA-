b0b8bc9b3eae923b731f2fadf2d88b733582df8d
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-11-14 18:19:45 -0300

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use((req,res,next) => {
   res.header('Access-Control-Allow-Origin', "*")
-  res.header("Access-Control-Allow-Header", "Content-Type")
+  res.header("Access-Control-Allow-Header", "Content-Type, Accept, Origin")
   if(req.method === 'OPTIONS'){
     res.header('Access-Control-Allow-Methods', "PUT, POST, GET, PATCH, DELETE")
     return res.status(200).send({})
