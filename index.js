6f5e1e5653e25eb52627cda1a2ef57ac3f000bc8
Nilton Oliveira <89275369+Nil-ton@users.noreply.github.com>
2021-11-14 17:26:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5500',
-  credentials: true
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
 }))
 
 app.use(express.json())
