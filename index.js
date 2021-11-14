0d5fbfcfab6fd2cb4d691b9706f997ea5fca4751
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-11-14 18:17:36 -0300

    .

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use((req,res,next) => {
     res.header('Access-Control-Allow-Methods', "PUT, POST, GET, PATCH, DELETE")
     return res.status(200).send({})
   }
+  app.use(cors())
   next()
 })
 
