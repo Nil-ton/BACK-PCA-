a8be14facabfce36698afd708f6e8bed586ff2b3
Nilton Oliveira <nilton.oliveira0305@gmail.com>
2021-11-14 18:27:57 -0300

    .

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use((req,res,next) => {
+  res.header('Access-Control-Allow-Origin', "*")
+  res.header('Access-Control-Allow-Methods', "PUT, POST, GET, PATCH, DELETE")
+  app.use(cors())
+  next()
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
